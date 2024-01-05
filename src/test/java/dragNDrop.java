49d32b0116fbc55c865c423c6f35a6e720c54adf
vy4cheslav <d4m1enpwnz@gmail.com>
2024-01-05 16:33:44 +0300

    Second part ready

---
+++ src/test/java/dragNDrop.java
@@ -0,0 +1,33 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.actions;
+
+public class dragNDrop {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void solutionDragNDrop(){
+        Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement column1 = $("#column-a");
+        SelenideElement column2 = $("#column-b");
+        $("#column-a").shouldHave(Condition.text("A"));
+        $("#column-b").shouldHave(Condition.text("B"));
+       // actions().clickAndHold(column1).moveToElement(column2).release().build().perform();
+
+       actions().dragAndDrop(column1, column2).build().perform();
+        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+
+    }
+}
