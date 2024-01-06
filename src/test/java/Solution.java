bf2aa7262585bbd36cbbc68d6fb5a7063bc88542
vy4cheslav <d4m1enpwnz@gmail.com>
2024-01-06 15:01:21 +0300

    Ready

--- src/test/java/Solution.java
+++ src/test/java/Solution.java
@@ -1,33 +1,29 @@
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.conditions.Text;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
-import static java.nio.channels.Selector.open;
+
 
 public class Solution {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.timeout = 5000; // default 4000
     }
 
     @Test
     void solutionEnterprizeHover(){
       Selenide.open("https://github.com");
-        $$(".div.HeaderMenu").filterBy(text("Solutions")).first().hover();
+      $$(".HeaderMenu-link").filterBy(text("\n" +
+              "        Solutions\n" +
+              "        ")).first().hover();
+      $$(".HeaderMenu-dropdown-link").filterBy(text("Enterprise")).first().click();
+      $("#hero-section-brand-heading").shouldHave(text("The AI-powered"));
 
-
-        //На главной странице github выберите меню solution - enterprize с помощью команды hover для solution
-        //Убедитесь что загрузилас нужная страница
     }
 }
