fff54170e796598a92a2bfc2ac12762d1db2b1e5
vy4cheslav <d4m1enpwnz@gmail.com>
2024-01-06 16:05:48 +0300

    Исправления

--- src/test/java/DragNDrop.java
+++ src/test/java/DragNDrop.java
@@ -8,12 +8,13 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.actions;
 
-public class dragNDrop {
+public class DragNDrop {
     @BeforeAll
     static void beforeAll(){
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000; // default 4000
+        Configuration.holdBrowserOpen = true;
     }
 
     @Test
@@ -21,13 +22,16 @@ public class dragNDrop {
         Selenide.open("https://the-internet.herokuapp.com/drag_and_drop");
         SelenideElement column1 = $("#column-a");
         SelenideElement column2 = $("#column-b");
-        $("#column-a").shouldHave(Condition.text("A"));
-        $("#column-b").shouldHave(Condition.text("B"));
-       // actions().clickAndHold(column1).moveToElement(column2).release().build().perform();
+//        $("#column-a").shouldHave(Condition.text("A"));
+//        $("#column-b").shouldHave(Condition.text("B"));
+//        actions().clickAndHold(column1).moveToElement(column2).release().build().perform();
 
-       actions().dragAndDrop(column1, column2).build().perform();
+        actions().dragAndDrop(column1, column2).build().perform();
+//        $("#column-a").dragAndDropTo("#column-b");
         $("#column-a").shouldHave(Condition.text("B"));
         $("#column-b").shouldHave(Condition.text("A"));
+
+
 
     }
 }
